1ff92df4ad76544631642397a2aeea3ca5b50555
Naufal Adli santosa <adlisantosanaufal@gmail.com>
2024-11-04 15:56:59 +0700

    ok

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.get('/', (req, res) => {
 });
 
 // Menjalankan server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+
